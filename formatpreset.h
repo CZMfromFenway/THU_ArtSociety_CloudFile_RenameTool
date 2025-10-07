9cbfd06f6332303b3469d5ca3be5aa642fefae4f
CZM <chengzm23@mails.tsinghua.edu.cn>
2025-10-07 19:10:56 +0800

    增加规范化注释

--- formatpreset.h
+++ formatpreset.h
@@ -1,6 +1,15 @@
 #ifndef FORMATPRESET_H
 #define FORMATPRESET_H
 
+/******************************************************************************
+ * @file       formatpreset.h
+ * @brief      含格式预设类
+ *
+ * @author     czm<chengzm23@mails.tsinghua.edu.cn>
+ * @date       2025/10/07
+ * @history    1.0
+ *****************************************************************************/
+
 #include <QString>
 #include <QRegularExpression>
 #include <QDateTime>
@@ -9,21 +18,51 @@
 #define TYPE_VID 0x02
 #define TYPE_DOC 0x04
 
+/**
+ * @brief 格式预设类
+ */
 class FormatPreset
 {
 public:
 
+    /**
+     * @brief 构造函数
+     * @param name                  预设名称
+     * @param format                预设格式
+     * @param content               预设内容
+     * @param type                  预设所修改文件类型
+     * @param customType            预设所自定义修改文件类型
+     */
     FormatPreset(const QString name, const QString& format, const QString& content, const int &type = 0, const QString& customType = ".*");
 
-    // 获取预设名
+    /**
+     * @brief 获取预设名称
+     * @return 预设名称
+     */
     QString const getName();
-    // 获取格式
+
+    /**
+     * @brief 获取预设格式
+     * @return 预设格式
+     */
     QString const getFormat();
-    // 获取默认内容
+
+    /**
+     * @brief 获取预设默认内容
+     * @return 预设默认内容
+     */
     QString const getDefaultContent();
-    // 获取匹配类型
+
+    /**
+     * @brief 获取预设所修改文件类型
+     * @return 类型（参考宏TYPE_XXX）
+     */
     int getType();
-    // 获取自定义类型
+
+    /**
+     * @brief 获取预设所自定义修改文件类型
+     * @return 扩展名过滤器
+     */
     QString const getCustomType();
 
 private:
